8b91fc0dfa18d1a97f9d2dc9852ac1eea2ff89ed
Daniel Horvath <horv.dan94@gmail.com>
2024-05-17 10:18:21 +0200

    feat

--- Backend/model/model.go
+++ Backend/model/model.go
@@ -6,3 +6,15 @@ type MessageSend struct {
 	Email     string
 	Message   string
 }
+
+type Product struct {
+	ID        string `json:"product_id"`
+	Name      string `json:"name"`
+	Price     int    `json:"price"`
+	Size      string `json:"size"`
+	Material  string `json:"material"`
+	Color     string `json:"color"`
+	Origin    string `json:"origin"`
+	Cleaning  string `json:"cleaning"`
+	ImagePath string `json:"imagePath"`
+}
